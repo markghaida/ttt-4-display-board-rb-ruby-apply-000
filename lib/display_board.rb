3dae1e19d172930800db5bc46f1d90ac541b1f15
Mark G <markghaida@gmail.com>
2020-09-30 20:43:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,13 +3,6 @@
 
 board = ["   ", "     ", "    ", "    ", "    ", "    " , "     ", "    ", "    "]
          
-def display_board (board)
-  
-  puts "    |#{rows[0]}|   " 
-  puts " -----------"#{rows[1]
-  puts "    |   |   "#{rows[2]
-  puts " -----------"#{rows[3]
-  puts "    |   |   "#{rows[4]
-end
+
 
 display_board(board)
