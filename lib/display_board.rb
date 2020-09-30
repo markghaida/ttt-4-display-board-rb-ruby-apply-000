fa7f128420759dff5393fd4f9436def451188e05
Mark G <markghaida@gmail.com>
2020-09-30 20:26:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board =  ["    |    |    ", "-----------", "    |    |    ", "-----------", "    |    |    "]
+         
+def display_board (rows)
+  puts #{rows[0]
+  puts #{rows[1]
+  puts #{rows[2]
+  puts #{rows[3]
+  puts #{rows[4]
+end
+
+display_board(board)
